3ebbfa6cedf68a6cd455773619e25ab231f2857d
Gonzalo Cantarero <gonzalo.cantarero.galvez@gmail.com>
2017-10-07 00:09:57 +0200

    Added firebase data to project.

--- src/app/service/information.service.ts
+++ src/app/service/information.service.ts
@@ -6,14 +6,28 @@ import { Http } from "@angular/http";
 export class InformationService {
 
   info:any = {};
+  about:any = {};
   isInfoLoaded:boolean = false;
+  isAboutLoaded:boolean = false;
+  team:any = [];
 
   constructor(public http:Http) {
+    this.loadInfo();
+   }
+
+   public loadInfo(){
     this.http.get("assets/data/info.page.json")
     .subscribe(data=>{
-      console.log(data.json());
       this.info = data.json();
       this.isInfoLoaded = true;
+    });
+   }
+
+   public loadAbout(){
+    this.http.get("https://tutorialangular4.firebaseio.com/equipo.json")
+    .subscribe(data=>{
+      this.about = data;
+      this.isAboutLoaded = true;
     });
    }
 
